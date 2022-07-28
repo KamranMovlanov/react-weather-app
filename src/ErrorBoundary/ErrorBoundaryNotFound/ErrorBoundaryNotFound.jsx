579752f8788220d43a78c9e11705ccc4310287e1
Kamran <kamran1992051432@gmail.com>
2022-07-28 15:29:43 +0300

    Changed link path

--- src/ErrorBoundary/ErrorBoundaryNotFound/ErrorBoundaryNotFound.jsx
+++ src/ErrorBoundary/ErrorBoundaryNotFound/ErrorBoundaryNotFound.jsx
@@ -22,7 +22,7 @@ class ErrorBoundaryNotFound extends Component {
       this.props.children[1].props.setErrStatus(true);
       return (
         <div className={style.AlternativeSearchResult}>
-          <Link className={style.currentWeatherBtn} reloadDocument to={"/current"}>
+          <Link className={style.currentWeatherBtn} reloadDocument to={"/"}>
             Назад
           </Link>
           <div className='backgroundWrapper'>
